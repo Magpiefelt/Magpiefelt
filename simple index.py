df73d935b6b07d9e6c3b67c123eefc059a0bf161
Magpiefelt <cohenmcleod@ymail.com>
2025-04-15 15:07:22 -0600

    Delete simple index.py

--- simple index.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, Response
-app = Flask(__name__)
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("Hello from Magpie Felt!", mimetype="text/plain")
