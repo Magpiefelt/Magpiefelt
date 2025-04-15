30795fcaf2444203421f366f771cbc7435d3b849
Magpiefelt <cohenmcleod@ymail.com>
2025-04-15 13:10:59 -0600

    Update settings.py

--- magpie_crafts/settings.py
+++ magpie_crafts/settings.py
@@ -75,9 +75,28 @@ MIDDLEWARE = [
 ]
 # Oscar settings
 OSCAR_SHOP_NAME = 'Magpie Felt'
+OSCAR_SHOP_TAGLINE = 'Handmade Wool Felting Kits and Art'
 OSCAR_DEFAULT_CURRENCY = 'CAD'
+OSCAR_FROM_EMAIL = 'noreply@magpiefelt.ca'
 OSCAR_DYNAMIC_CLASS_LOADER = 'oscar.core.loading.default_class_loader'
 OSCAR_REQUIRED_ADDRESS_FIELDS = ('first_name', 'last_name', 'line1', 'city', 'country', 'postcode')
+OSCAR_SLUG_ALLOW_UNICODE = False
+OSCAR_SLUG_MAP = None
+OSCAR_SLUG_FUNCTION = 'oscar.core.utils.default_slugifier'
+OSCAR_HIDDEN_FEATURES = []
+OSCAR_MISSING_IMAGE_URL = MEDIA_URL + 'image_not_found.jpg'
+OSCAR_UPLOAD_ROOT = os.path.join(MEDIA_ROOT, 'uploads/')
+OSCAR_COOKIES_SECURE = False
+OSCAR_COOKIES_HTTPONLY = True
+OSCAR_USE_LESS = False
+OSCAR_USE_DATADOG = False
+OSCAR_ALLOW_ANON_CHECKOUT = True
+OSCAR_ALLOW_ANON_REVIEWS = True
+OSCAR_MODERATE_REVIEWS = False
+OSCAR_SEND_REGISTRATION_EMAIL = True
+OSCAR_EAGER_ALERTS = False
+OSCAR_LOGIN_REDIRECT_URL = '/'
+OSCAR_DASHBOARD_DEFAULT_ACCESS_FUNCTION = 'oscar.apps.dashboard.nav.default_access_fn'
 
 # Haystack settings (required by Oscar)
 HAYSTACK_CONNECTIONS = {
